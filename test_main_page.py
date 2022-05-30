11c35183c5fb7e7893513ee195493fb9dc4b63dd
“Alexey“ <nvmqwer593@gmail.com>
2022-05-30 22:36:44 +0300

    add new feature

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 from pages.main_page import MainPage
 
+LINK = "http://selenium1py.pythonanywhere.com/"
+
 
 def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
@@ -8,14 +10,12 @@ def go_to_login_page(browser):
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, LINK)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = MainPage(browser, LINK)
     page.open()
     page.should_be_login_link()
